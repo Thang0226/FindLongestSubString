e3b7587208e0f7aba39a9b970ad8919d0290f655
Thang0226 <thang.nd0226@gmail.com>
2024-10-25 16:11:42 +0700

    add time complexity calculation

--- src/LongestSubString.java
+++ src/LongestSubString.java
@@ -59,3 +59,23 @@ public class LongestSubString {
 		return outputString;                                    // 19
 	}
 }
+
+/**     Tính toán độ phức tạp thời gian của phương thức findSubString
+ *      - Các lệnh {1, 2, 3, 4, 5, 6, 16, 19} đều có độ phức tạp O(1)
+ *      - Lệnh 17 thực hiện lặp lệnh 18 x lần, x <= n, lệnh 18 độ phức tạp O(1)
+ *          ==> Độ phức tạp vòng lặp dòng {17, 18}: O(n) (case xấu nhất)
+ *      - Vòng lặp dòng {7, 8, 9, 10, 11, 12, 13, 14, 15}:
+ *          + Số lần lặp: n
+ *          + Tổng phép tính các biểu thức điều kiện và lệnh ở {8, 9, 10, 11, 15} có độ phức tạp O(1)
+ *          + lệnh 13 và 14 giống nhau, gồm một số x bước O(1), x là tổng số lần lệnh addLast() đã thực hiện trước
+ *          đó kể từ lần list rỗng gần nhất.
+ *          Vì addLast() luôn thực hiện 1 lần trong 1 vòng lặp (không hơn không kém), tổng x trong cả chương trình
+ *          không vượt quá n --> tổng số phép tính của {13, 14} là O(n).
+ *          + lệnh 12 gồm một số y bước O(1), y bằng với số bước của lệnh 13 ở một thời điểm trước đó. Vì vậy tổng y
+ *          trong cả chương trình không vượt quá tổng x trong cả chương trình --> số phép tính của lệnh 12 nhỏ hơn O(n).
+ *          --> Độ phức tạp của {12, 13, 14} là O(n).
+ *          ==> Độ phức tạp của vòng lặp: O(n)
+ *
+ *       ==> Độ phức tạp của chương trình: T(n) = O(1) + O(n) + O(n) = O(n)
+ *
+ */
